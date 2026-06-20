be3498ff617e94153e9114ce5f436e697836411f
Neko-Kuroi <88179359+Neko-Kuroi@users.noreply.github.com>
2026-06-20 14:59:08 +0000

    watermark function

--- mangaupload_yomima_site/yomima/manga.py
+++ mangaupload_yomima_site/yomima/manga.py
@@ -26,6 +26,11 @@ from typing import Optional
 
 from PIL import Image
 from natsort import natsorted
+#from watermark import embed_watermarks, ip_to_seed_b
+# scramble(seed_B) 適用後に呼ぶ
+#seed_b = ip_to_seed_b(client_ip)
+#img = embed_watermarks(img, seed_b, client_ip)
+
 
 # ---------------------------------------------------------------------------
 # Constants
